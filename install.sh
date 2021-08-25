4b860437c8dc212da1027af98adc5b41f2d84247
mxldarkness <86469258+mxldarkness@users.noreply.github.com>
2021-08-25 17:25:20 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pkg update && pkg upgrade
-pkg install python
-pkg install python-pip
-pip install colorama
-pip install fake-useragent
-echo("Устоновка прошла успешно :D")
-fi
-python3 bomber.py 
